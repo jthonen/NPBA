857fbcd3a40ffc30597bf934ee940939338b5f16
peak <thonen.jacob@gmail.com>
2019-01-29 17:37:30 -0800

    Lets get this started

---
+++ client/src/components/SignUp/index.js
@@ -0,0 +1,42 @@
+import React from "react";
+import "./style.css";
+
+function SignUp() {
+    return (
+        <div>
+            <ul class="slideshow">
+                <li></li><li></li><li></li><li></li><li></li>
+                <li></li><li></li><li></li><li></li><li></li>
+                <li></li><li></li><li></li><li></li><li></li>
+                <li></li><li></li><li></li><li></li><li></li>
+                <li></li><li></li><li></li><li></li><li></li>
+            </ul>
+
+            <form id="msform" action="https://www.enformed.io/gh0zckmw" method="POST">
+                <fieldset>
+                    <div id="main">
+                        <h2 class="fs-title">Create your Admin Account</h2>
+                        <div id="login">
+                    
+                            <h3 class="fs-subtitle">Name</h3>
+                            <input type="text" name="name" id="name" required="required" placeholder="Please Enter Name"/><br /><br />
+                                
+                            <h3 class="fs-subtitle">Username</h3>
+                            <input type="text" name="uname" id="Username" required="required" placeholder="Username"/><br /><br />
+                            
+                            <h3 class="fs-subtitle">Email</h3>
+                            <input type="email" name="email" id="email" required="required" placeholder="john123@gmail.com"/><br/><br />
+                            
+                            <h3 class="fs-subtitle">Password</h3>
+                            <input type="password" name="password" id="city" required="required" placeholder="Please Enter Password"/><br/><br />
+            
+                            <input action = "" type="submit" value="Submit" class="submit action-button" name="submit"/><br />
+                        </div>
+                    </div>
+                </fieldset>
+            </form>    
+        </div>
+    );
+}
+
+export default SignUp;
