eafef5b463f9fb770f844ca28d9b6987c86bded4
Erik Cedillo <erikcedillo@Eriks-MacBook-Pro.local>
2019-01-29 19:48:47 -0800

    The Nav is now working

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,3 +1,4 @@
+
 //PAGE LOADER//
 $(document).ready(function(){
     setTimeout(function(){
@@ -5,3 +6,17 @@ $(document).ready(function(){
 
     }, 3000);
 });
+// Setting up the Variables
+var bars = document.getElementById("nav-action");
+var nav = document.getElementById("nav");
+
+
+//setting up the listener
+bars.addEventListener("click", barClicked, false);
+
+
+//setting up the clicked Effect
+function barClicked() {
+  bars.classList.toggle('active');
+  nav.classList.toggle('visible');
+}
