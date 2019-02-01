cc9ff19c82fe5c36dd7482187ea379ed744f33d1
Your Name <you@example.com>
2019-02-01 13:47:39 -0800

    UserHand component mvp

--- client/src/components/PlayerCard/index.js
+++ client/src/components/PlayerCard/index.js
@@ -2,10 +2,10 @@ import React from "react";
 import "./style.css";
 
 function PlayerCard(props) {
-    console.log(props);
+    console.log(props.PlayerCardInHand.team_style);
     return (
         <div className="PlayerCard" id={props.PlayerCardInHand.name}>
-            <div className="CardBackground">
+            <div className="CardBackground" style={props.PlayerCardInHand.team_style}>
                 <img className="TeamLogo" alt={props.PlayerCardInHand.logo+" as displayed on card"} src={props.PlayerCardInHand.logo} />
                 <img className="PlayerImage" alt={props.PlayerCardInHand.name+" image"} src={props.PlayerCardInHand.image} />                
             </div>
