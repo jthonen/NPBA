05401570649c48681d74b73f39afb15294e0dddd
Your Name <you@example.com>
2019-02-07 18:21:11 -0800

    added gameviewport to react router again @ /play

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import LogInPage from "./pages/LogInPage";
+import GameViewport from "./pages/GameViewport";
 
 function App() {
   return (
     <Router>
         <Switch>
           <Route exact path="/" component={LogInPage} />
+          <Route exact path="/play" component={GameViewport} />
         </Switch>
     </Router>
   );
